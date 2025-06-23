d635218dbf66627485ad2d0f0218bf84d01eca2d
prakash_07 <prakashranjan8454@gmail.com>
2025-06-23 18:05:08 +0530

    index.js

--- server/index.js
+++ server/index.js
@@ -1,25 +1,18 @@
-const express = require("express");
-const app = express();
 
-require("dotenv").config();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8000;
 
-const cors = require("cors");
+// Middlewares
 app.use(cors({
   origin: "http://localhost:3000",
   credentials: true
 }));
-
-const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 app.use(express.json());
 
-// DB Connection
-const { connect } = require("./config/database");
+// Connect to DB
 connect();
 
 // Routes
-const userRoutes = require("./routes/userRoute");
 app.use("/api/v1", userRoutes);
 
 // Start server
